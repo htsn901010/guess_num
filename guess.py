d9dd89de7b59a0879d385b257abc9934b93553b6
Ziv Chang <htsn901010@gmail.com>
2022-04-25 21:49:36 -0500

    add start and end numbers'

--- guess.py
+++ guess.py
@@ -1,5 +1,10 @@
 import random
-r = random.randint(1, 100)
+start = input('enter your start number: ')
+end = input('enter your end number: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1
