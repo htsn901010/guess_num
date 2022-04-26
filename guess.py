e8f421e33c2781fca676dae3c0890b616db6cd6b
Ziv Chang <htsn901010@gmail.com>
2022-04-25 21:43:22 -0500

    add account'

--- guess.py
+++ guess.py
@@ -1,12 +1,16 @@
 import random
-r = random.randint(1,100)
+r = random.randint(1, 100)
+count = 0
 while True:
-    num = input('enter your number: ')
-    num = int(num)
-    if num == r:
-        print('you guess right!')
-        break
-    elif num < r:
-        print('your number is smaller than the answer.')
-    elif num > r:
-        print('your number is larger than the answer.')
+	count = count + 1
+	num = input('enter your number: ')
+	num = int(num)
+	if num == r:
+		print('your guess is right!')
+		print('this is your', count, 'time try.')
+		break
+	elif num < r:
+		print('your number is smaller than the answer.')
+	elif num > r:
+		print('your number is larger than the answer.')
+	print('this is your', count, 'time try.')
